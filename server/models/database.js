5defa3e1d767dd5cbeaba2bad53b9d26816ec2b2
Rishiponnam <ponnamrishith887@gmail.com>
2024-12-03 22:00:09 +0530

    commit

--- server/models/database.js
+++ server/models/database.js
@@ -1,8 +1,8 @@
 const mongoose=require("mongoose");
 require('dotenv').config({ path: '.env' });
 
-const uri=process.env.MONGODB_URI;
-// console.log(uri);
+const uri="mongodb://localhost:27017/";
+console.log(uri);
 mongoose.connect(uri,{ useNewUrlParser: true });
 const connection=mongoose.connection;
 connection.once('open', () => {
